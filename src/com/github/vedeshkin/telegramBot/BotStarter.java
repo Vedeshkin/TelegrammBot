66730dad71d6182b6e29a7624067b6c2466dd5e2
vedeshkin <evilfunt@gmail.com>
2018-04-05 18:48:45 +0300

    Second commit. Added support of for maven

--- src/com/github/vedeshkin/telegramBot/BotStarter.java
+++ src/com/github/vedeshkin/telegramBot/BotStarter.java
@@ -1,14 +1,12 @@
 package com.github.vedeshkin.telegramBot;
 
 /**
- * Created by vvedeshkin on 4/5/2018.
+ * Created by Vedeshkin on 4/5/2018.
+ * All right reserved.
  */
 public class BotStarter {
 
-
-
-    private void getProperties(){};
-
+    private void getProperties(){}
 
 
 }
